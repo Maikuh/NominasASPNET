0ce4ad55acdf7d06592ede7255a1cdd42edcc9f9
Mike Araujo <miguelant.araujo@gmail.com>
2018-07-26 06:49:18 -0400

    Changed horario's datetime format and started styling create empleados form.

--- Nominas/Horario.cs
+++ Nominas/Horario.cs
@@ -7,6 +7,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Nominas
 {
     using System;
@@ -21,8 +23,12 @@ namespace Nominas
         }
     
         public int Codigo_Horario { get; set; }
-        public System.DateTime Hora_Inicio { get; set; }
-        public System.DateTime Hora_Fin { get; set; }
+
+        [DataType(DataType.Time), DisplayFormat(DataFormatString = "{0:t}")]
+        public DateTime Hora_Inicio { get; set; }
+
+        [DataType(DataType.Time), DisplayFormat(DataFormatString = "{0:t}")]
+        public DateTime Hora_Fin { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Empleado> Empleado { get; set; }
